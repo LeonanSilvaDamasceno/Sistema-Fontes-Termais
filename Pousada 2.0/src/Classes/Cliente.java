61aadf7f55588b71ff83561fe298773238ca90c1
Luis Henrique Rodrigues de Oliveira <152280769+LuisHRodrigues@users.noreply.github.com>
2024-07-14 17:57:02 -0300

    Add files via upload

---
+++ Pousada 2.0/src/Classes/Cliente.java
@@ -0,0 +1,44 @@
+package Classes;
+
+public class Cliente extends Pessoa {
+	private int numQuarto;
+	private int tempoReserva;
+	Fonte_Termal fonte_termal;
+	
+	//Construtor padrão
+	public Cliente(String nome, String CPF, String email, int numQuarto, int tempoReserva) {
+		super(nome, CPF, email);
+		this.numQuarto = numQuarto;
+		this.tempoReserva = tempoReserva;
+	}
+	
+	//Apartir daqui são os metodos getters e setters
+	public void setFonte_termal(Fonte_Termal fonte_termal) {
+		this.fonte_termal = fonte_termal;
+	}
+
+	public void setNumQuarto(int numQuarto) {
+		this.numQuarto = numQuarto;
+	}
+
+	public void setTempoReserva(int tempoReserva) {
+		this.tempoReserva = tempoReserva;
+	}
+
+	public int getTempoReserva() {
+		return tempoReserva;
+	}
+
+	public int getNumQuarto() {
+		return numQuarto;
+	}
+	
+	public void usarFonte() {
+		System.out.print("Cliente" + getNome() + "está usando a fonte \"" + fonte_termal.getCodFonte() + "\"!");
+	}
+	
+	//Metodo para renovar a reserva do cliente
+	public void renovarReserva(int tempoReserva) {
+
+	}
+}
---
+++ Pousada 2.0/src/Classes/Quarto.java
@@ -0,0 +1,27 @@
+package Classes;
+public class Quarto {
+    private int numQuarto;
+    private String disponibilidade; //Mudado para String devido a complexidades do BD
+
+  //Construtor padrão
+    public Quarto(int numQuarto, String disponibilidade) {
+        this.numQuarto = numQuarto;
+        this.disponibilidade = disponibilidade;
+    }
+    
+  //Apartir daqui são os metodos getters e setters
+    public void setNumQuarto(int numQuarto) {
+        this.numQuarto = numQuarto;
+    }
+    public void setDisp(String disponibilidade) {
+        this.disponibilidade = disponibilidade;
+    }
+
+    public String getDisp() {
+        return disponibilidade;
+    }
+    public int getNumQuarto() {
+        return numQuarto;
+    }
+
+}
